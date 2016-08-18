080be8ddf44e0c5d054d3e28af733090e01e4618
Andy Zhang <signupforandy@gmail.com>
2016-08-18 12:40:00 -0700

    WIP proto build

---
+++ app/src/main/java/chat/randomidea/com/chatterbox/ChatClient.java
@@ -0,0 +1,11 @@
+package chat.randomidea.com.chatterbox;
+
+/**
+ * Created by andy on 8/18/16.
+ */
+public class ChatClient {
+
+    public static void SayHello() {
+        System.out.print("SayHello");
+    }
+}
