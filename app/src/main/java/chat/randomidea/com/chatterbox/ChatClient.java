9061dae76cda860251d225b86f23c413521939b4
Andy Zhang <signupforandy@gmail.com>
2016-08-19 17:00:03 -0700

    Add forTarget client conn

--- app/src/main/java/chat/randomidea/com/chatterbox/ChatClient.java
+++ app/src/main/java/chat/randomidea/com/chatterbox/ChatClient.java
@@ -25,10 +25,9 @@ public class ChatClient {
     private final ChatGrpc.ChatStub asyncStub;
     private final StreamObserver<EventSubscription> eventSubscriptionStreamObserver;
 
-    public ChatClient(String host, int port) {
-        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true));
-        logger.info("Running chat client for" + host + ":" + port);
-
+    public ChatClient(String target) {
+        this(ManagedChannelBuilder.forTarget(target).usePlaintext(true));
+        logger.info("Running chat client for " + target);
     }
 
     /** Construct client for accessing RouteGuide server using the existing channel. */
